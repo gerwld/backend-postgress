9e4ef566d99774a28476422ec0d8dcd63a4993a9
PJaworski <pjaworski.dev@gmail.com>
2025-03-30 20:06:19 +0200

    Added JWT login, registration

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,22 +7,33 @@ import { RolesService } from 'src/roles/roles.service';
 @Injectable()
 export class UsersService {
 
-    constructor(@InjectModel(User) private userRepository: typeof User, 
-                private roleService: RolesService) {}
+    constructor(@InjectModel(User) private userRepository: typeof User,
+        private roleService: RolesService) { }
 
     async createUser(dto: CreateUserDto) {
         const user = await this.userRepository.create(dto);
         const role = await this.roleService.getRoleByValue('USER');
-        
-        if(role?.id) await user.$set('roles', [role.id]);
-        
+
+        if (role?.id) {
+            await user.$set('roles', [role.id]);
+            user.roles = [role];
+        }
+        await user.save();
         return user;
     }
 
     async getAllUsers() {
         const users = await this.userRepository.findAll({
-            include: {all: true}
-        });     
+            include: { all: true }
+        });
         return users;
+    }
+
+    async getUserByEmail(email: string) {
+        const user = await this.userRepository.findOne({ where: { email }, include: { all: true } })
+        
+        if (user) {
+            return user.get({ plain: true });
+        }
     }
 }
