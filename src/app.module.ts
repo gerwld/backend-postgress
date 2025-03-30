6d3b3ab5acab1fd675eab26fb87d599e0f34fbc4
PJaworski <pjaworski.dev@gmail.com>
2025-03-30 16:02:51 +0200

    Connected local postgres db

---
+++ src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from "@nestjs/common";
+import { SequelizeModule } from "@nestjs/sequelize";
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
+
+@Module({
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env'
+        }),
+        SequelizeModule.forRoot({
+            dialect: 'postgres',
+            host: process.env.POSTGRESS_HOST,
+            port: Number(process.env.POSTGRESS_PORT),
+            username: process.env.POSTGRESS_USER,
+            password: process.env.POSTGRESS_PWD,
+            database: process.env.POSTGRESS_DB,
+            models: [],
+            autoLoadModels: true
+          }),
+        UsersModule,
+    ]
+})
+export class AppModule {
+
+}
